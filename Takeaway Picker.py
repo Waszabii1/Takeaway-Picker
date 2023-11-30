de35c5a5c98b4e001f092c51036e0599e9f4fee3
Waszabii1 <148143917+Waszabii1@users.noreply.github.com>
2023-11-30 21:15:37 +0000

    Update Takeaway Picker.py

--- Takeaway Picker.py
+++ Takeaway Picker.py
@@ -18,6 +18,6 @@ for tag in range(5, (len(tags)-28)): #removes Nones and Social Medias
         pass
 choice = random.choice(resturants)
 amount = len(resturants)
-print("Out of " + str(amount) + " restrurants, the random resturant for " + postcode + " is "  + choice)
+print("Out of " + str(amount) + " restaurants, the random restaurant for " + postcode + " is "  + choice)
 
 
