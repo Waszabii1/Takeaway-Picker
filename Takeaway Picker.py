720c41273b2ab0077e47535db45642d3c53bbf55
Waszabii1 <148143917+Waszabii1@users.noreply.github.com>
2023-12-02 14:37:40 +0000

    Update Takeaway Picker.py
    added a reroll feature incase you dont want your random pick, goes against the whole thing but

--- Takeaway Picker.py
+++ Takeaway Picker.py
@@ -2,24 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 import random
 
+restaurants = []
 
-def main():
+
+def list_maker():
     url = f'https://deliveroo.co.uk/restaurants/london/westminster?postcode={postcode}&collection=all-restaurants'
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
     tags = doc.find_all("a")
 
-    resturants = []
     for tag in range(5, (len(tags) - 28)):  # removes Nones and Social Medias
         individual = tags[tag].get("aria-label")
         try:
-            resturants.append(individual)
+            restaurants.append(individual)
         except TypeError:
             pass
-    choice = random.choice(resturants)
-    amount = len(resturants)
+    
+    return restaurants
+
+def pick():
+    amount = len(restaurants)
+    choice = random.choice(restaurants)    
     print("Out of " + str(amount) + " restaurants, the random restaurant for " + postcode + " is " + choice)
+    re_roll()
+    
+def re_roll():
+    question = input("Want to pick again? (Y/N)")
+    global answer
+    answer = question.upper()
+    if answer == "Y":
+        answer = True
+    else:
+        answer = False
+    return answer
 
 
 if __name__ == '__main__':
@@ -28,4 +44,10 @@ if __name__ == '__main__':
         postcode = input("What is your postcode? ")
         if len(postcode) != 6:
             print("Please enter a valid postcode")
-    main()
+    list_maker()
+    pick()
+    while answer == True:
+        pick()
+        if answer == False:
+          print("Enjoy your meal!")
+    
